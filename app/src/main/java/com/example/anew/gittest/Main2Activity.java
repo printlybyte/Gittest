e850e1e02af4b4d615e51dc0bd98262ddd0727b5
liuguodong <1226675335@qq.com>
2017-03-09 18:14:46 +0800

    comit  @liuguodong

--- app/src/main/java/com/example/anew/gittest/Main2Activity.java
+++ app/src/main/java/com/example/anew/gittest/Main2Activity.java
@@ -26,7 +26,7 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_test:
-                Toast.makeText(this, "d", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "da", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
